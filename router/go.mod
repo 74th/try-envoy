6ef4bf501d28d995c581911fe2bcd9892b399335
74th <site@74th.tech>
2020-04-26 08:00:05 +0900

    go docker build

---
+++ router/go.mod
@@ -0,0 +1,8 @@
+module github.com/74th/try-envoy/router
+
+go 1.14
+
+require (
+	github.com/golang/protobuf v1.4.0
+	google.golang.org/grpc v1.29.1
+)
--- tasks.py
+++ tasks.py
@@ -4,3 +4,8 @@ from invoke import task
 def update_protobug_service(c):
     with c.cd("./router"):
         c.run("protoc ./router.proto --go_out=plugins=grpc:./")
+
+@task
+def build(c):
+    c.run("docker-compose build")
+    c.run("docker-compose push")
